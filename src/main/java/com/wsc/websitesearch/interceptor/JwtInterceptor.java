9008f356f8ceaf62902af7106f33f407f81036bd
王世成 <wangshicheng0815@126.com>
2019-10-27 17:26:13 +0800

    jwt demo

---
+++ src/main/java/com/wsc/websitesearch/interceptor/JwtInterceptor.java
@@ -0,0 +1,16 @@
+package com.wsc.websitesearch.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+}
